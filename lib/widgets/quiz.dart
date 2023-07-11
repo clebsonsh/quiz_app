563505789c5cdf25a15de70c4ca9ab910cbba033
Clebson Moura <clebson.sh@gmail.com>
2023-07-11 11:55:26 -0300

    Accepting & Passing Functions as Values

--- lib/widgets/quiz.dart
+++ lib/widgets/quiz.dart
@@ -11,7 +11,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget activeScreen = const StartScreen();
+  Widget activeScreen = const StartScreen(switchScreen);
 
   void switchScreen() {
     setState(() {
