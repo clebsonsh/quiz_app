bc8e843f4e11d0e7cc4b78b07416312a0a6bd4a9
Clebson Moura <clebson.sh@gmail.com>
2023-07-12 14:08:21 -0300

    Accessing List Elements & Object Properties

--- lib/widgets/questions_screen.dart
+++ lib/widgets/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/models/quiz_question.dart';
 import 'package:quiz_app/widgets/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -11,19 +13,24 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final QuizQuestion currentQuestion = questions[0];
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'Question text...',
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.white,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                fontSize: 24,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
             ),
           ),
           AnswerButton(
-            answerText: 'Qualé?',
+            answerText: currentQuestion.answers[0],
             onTap: () {},
           ),
         ],
